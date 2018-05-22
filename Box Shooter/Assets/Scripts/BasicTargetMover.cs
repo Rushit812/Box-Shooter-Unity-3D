0289542104aa19aaf1a13e0b1825f3e5936dd0a8
Rushit812 <rushittrivedi8@gmail.com>
2018-05-22 18:45:09 +0530

    Box Shooter - Unity FPS shooting game
     A 3D first person shooting game where you shoot the surrounding boxes to earn points and obtain the point-target to reach the next level in limited time.

---
+++ Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update () {
+        // rotate around the X axis
+        if (doSpin)
+        {
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+        
+        if (doMotion)
+        {
+            // move up and down overtime
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+        
+	}
+}
